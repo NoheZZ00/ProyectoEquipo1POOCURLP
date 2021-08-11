1b4c2bfb4747ae2cde3af5c5af3beff19c1cdc40
Walter Maradiaga <Walter Maradiaga@WALTER_CARRANZA>
2021-08-11 01:37:11 -0600

    Se agrego la clase CDEmpleado, Conexion, CLEmpleado

---
+++ src/main/java/com/curlp/capalogica/CLEmpleado.java
@@ -0,0 +1,75 @@
+
+package com.curlp.capalogica;
+
+public class CLEmpleado {
+    String DNIEmp;
+    String nombreEmp;
+    String apellidosEmp;
+    String direccion;
+    String celular;
+    int idCargo;
+    int idCiudad;
+
+    public String getDNIEmp() {
+        return DNIEmp;
+    }
+
+    public void setDNIEmp(String DNIEmp) {
+        this.DNIEmp = DNIEmp;
+    }
+
+    public String getNombreEmp() {
+        return nombreEmp;
+    }
+
+    public void setNombreEmp(String nombreEmp) {
+        this.nombreEmp = nombreEmp;
+    }
+
+    public String getApellidosEmp() {
+        return apellidosEmp;
+    }
+
+    public void setApellidosEmp(String apellidosEmp) {
+        this.apellidosEmp = apellidosEmp;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    public int getIdCiudad() {
+        return idCiudad;
+    }
+
+    public void setIdCiudad(int idCiudad) {
+        this.idCiudad = idCiudad;
+    }
+
+    public CLEmpleado() {
+        
+    }
+    
+    
+    
+}
