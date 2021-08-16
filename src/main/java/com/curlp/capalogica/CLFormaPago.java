12e57a72b6d8be590eb4d5c96dcda641bae9a6c7
Owner User <Owner User@Iveth-Linarez>
2021-08-16 12:49:05 -0500

    Se agregó clase CLForma pago

---
+++ src/main/java/com/curlp/capalogica/CLFormaPago.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.capalogica;
+
+/**
+ *
+ * @author Owner User
+ */
+public class CLFormaPago {
+    
+    private int idFormaPago;
+    private String formaPago;
+
+    public int getIdFormaPago() {
+        return idFormaPago;
+    }
+
+    public void setIdFormaPago(int idFormaPago) {
+        this.idFormaPago = idFormaPago;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+    
+    
+    
+}
